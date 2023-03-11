a3277a0822c825c3e00c9324d7748d12933b89e5
AGU0108 <109751630+AGU0108@users.noreply.github.com>
2023-03-11 13:49:26 -0600

    Add files via upload

---
+++ Actividad 113/Intro-To-p5/main.js
@@ -0,0 +1,32 @@
+function preload(){
+
+}
+
+function setup(){
+canvas = createCanvas(600,500);
+canvas.position(400,250);
+
+video = createCapture(VIDEO);
+video.hide();
+
+tint_color = "";
+
+
+
+}
+
+function draw(){
+    image(video, 0,0, 600,500);
+    tint(tint_color);
+
+}
+
+function filter_tint(){
+tint_color = document.getElementById("color_name").value;
+
+}
+
+function take_snapshot(){
+save('foto_cool.png');
+}
+
